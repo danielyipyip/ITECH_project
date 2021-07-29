7b0a15d2bba5f58531d9539e5ea42e59215cc58c
Daniel YL YIP <2602505y@student.gla.ac.uk>
2021-07-29 21:13:34 +0100

    CH9 exercise complete

--- rango/views.py
+++ rango/views.py
@@ -41,6 +41,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     # A HTTP POST?
@@ -57,6 +58,7 @@ def add_category(request):
     # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request,category_name_slug):
     #need to test is category exist first
     try:
@@ -133,7 +135,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 @login_required #only loged in can log out
 def user_logout(request):
