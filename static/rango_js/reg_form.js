23270b2239d039839aef6cb9393eca3e20d37122
Tsz Yui Tsang <2573971t@student.gla.ac.uk>
2021-08-06 20:34:06 +0100

    moved some internal js/css to external

---
+++ static/rango_js/reg_form.js
@@ -0,0 +1,9 @@
+var passwordvalidate = function() {
+    if (document.getElementById('id_password1').value == document.getElementById('id_password2').value) {
+           document.getElementById('validationmessage').style.color = 'green';
+           document.getElementById('validationmessage').innerHTML = 'Password Matched';
+   } else {
+           document.getElementById('validationmessage').style.color = 'red';
+           document.getElementById('validationmessage').innerHTML = 'Password not match';
+           }
+}
